98fb5e8729cdb3e2458456c4e20476084c4cfb50
harusys <56357240+harusys@users.noreply.github.com>
2022-10-16 13:17:32 +0000

    Change template

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,19 +1,30 @@
 import { createApp } from "vue";
-import { Quasar } from "quasar";
+import { Quasar, Dialog } from "quasar";
 import quasarLang from "quasar/lang/ja";
-
-import "@quasar/extras/material-icons/material-icons.css";
-
-import "quasar/src/css/index.sass";
+import router from "./router";
+// import { key, store } from "./store";
 
 import App from "./App.vue";
+
+// Import icon libraries
+import "@quasar/extras/roboto-font/roboto-font.css";
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
 
 const myApp = createApp(App);
 
 // Quasar 導入
 myApp.use(Quasar, {
-  plugins: {},
-  lang: quasarLang,
+	plugins: { Dialog }, // import Quasar plugins and add here
+	lang: quasarLang
 });
+
+// Vue Router 導入
+myApp.use(router);
+
+// Vuex 導入
+// myApp.use(store, key);
 
 myApp.mount("#app");
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
-import path from "path";
 
 export default defineConfig({
-  // src 配下を @ で import できるようにする
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  // Quasar 導入
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    quasar({
-      sassVariables: "src/quasar-variables.sass",
-    }),
-  ],
+	// Quasar 導入
+	plugins: [
+		vue({
+			template: { transformAssetUrls },
+		}),
+		quasar({
+			sassVariables: "src/quasar-variables.sass",
+		}),
+	],
 });
