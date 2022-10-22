98d68b15d99d4d720a63d0faa043e59befe4121e
Gen Suzuki <48849186+gsuzuki983@users.noreply.github.com>
2022-10-22 16:13:52 +0000

    add comment

--- api/TodosRead/index.js
+++ api/TodosRead/index.js
@@ -1,3 +1,18 @@
+// https://learn.microsoft.com/ja-jp/azure/azure-functions/functions-reference-node?tabs=v2-v3-v4-export%2Cv2-v3-v4-done%2Cv2%2Cv2-log-custom-telemetry%2Cv2-accessing-request-and-response%2Cwindows-setting-the-node-version#exporting-a-function
+// module.exports (または exports) を使用して、JavaScript 関数をエクスポートする必要があります。 
+// エクスポートする関数は、トリガーされたときに実行される JavaScript 関数である必要があります。
+// エクスポートされる関数には、実行時に多数の引数が渡されます。 受け取る最初の引数は常に、context オブジェクトです。
 module.exports = async function (context, req) {
-    context.res.json(context.bindings.documents);
+    // functions.json で定義しているdocuments≒CosmosDBからの検索結果をレスポンスに与える
+    // console.log(context.bindings.documents);
+    try {
+        context.res = {
+            status: 200,
+            body: await context.bindings.documents
+        };
+    } catch (err) {
+        context.log.error('CosmosDBの情報取得に失敗しました', err);
+        // This rethrown exception will be handled by the Functions Runtime and will only fail the individual invocation
+        throw err;
+    }
 }
--- api/TodosUpdate/index.js
+++ api/TodosUpdate/index.js
@@ -1,12 +1,16 @@
 module.exports = async function (context, req) {
     // 簡易バリデーション（Update想定なのでidもチェック）
+    // JSの主流の型判定：Object.prototype.toString.call()
     if (!req.body.id || !req.body.description || Object.prototype.toString.call(req.body.done) !== '[object Boolean]') {
-        context.res = { status: 400 };
+        context.res = { 
+            status: 400,
+            body: "不正なリクエストです"
+        };
         context.done(); // 明示的に終了
     }
-
+    
     // 出力バインドにリクエストBodyを渡す
     context.bindings.todoDocument = req.body;
 
-    context.res = { status: 204 };
+    context.res = { status: 204 };    
 };
